a2b29c2c1e37acae63e1026570c1854b2ee7e944
Sara Soares <sarinhapachecosoares@gmail.com>
2026-08-03 19:36:15 -0300

    hash

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = Router();
+
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "Nome, email e senha são obrigatórios." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return res.status(201).json({
+        message: "Usuário registrado com sucesso.",
+        user: {
+            name,
+            email,
+            password: hashedPassword
+        },
+    });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email e senha são obrigatórios" });
+  }
+
+  const fakeUser = {
+    email: "teste@email.com",
+    password: await bcrypt.hash("123456", 10),
+  };
+
+  const validPassword = await bcrypt.compare(password, fakeUser.password);
+
+  if (!validPassword || fakeUser.email !== email) {
+    return res.status(401).json({ message: "Credenciais inválidas" });
+  }
+
+  const token = jwt.sign({ email }, "secret", { expiresIn: "1h" });
+
+  return res.json({
+    message: "Login realizado com sucesso",
+    token,
+  });
+});
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import productsRouter from "./routes/products";
 import categoriesRouter from "./routes/categories";
+import authRouter from "./routes/auth";
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
 
+// Rotas
+app.use("/auth", authRouter);
 app.use("/products", productsRouter);
 app.use("/categories", categoriesRouter);
 
